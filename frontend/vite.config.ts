1057ddc3b2de46bb3e9872d05ff801ee78bd3b29
mfahmialif <mfahmialif@gmail.com>
2025-12-18 22:39:59 +0700

    Revert "backend change directory to ready deploy"
    This reverts commit 1e1487b3f69d1207bf00676228b24ad55c25be1d.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -21,28 +21,12 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      // "/api": {
-      //   target: "http://localhost:8000",
-      //   changeOrigin: true,
-      // },
-      // "/sanctum": {
-      //   target: "http://localhost:8000",
-      //   changeOrigin: true,
-      // },
-      // "/api": {
-      //   target: "http://localhost/tracert/backend/public_html",
-      //   changeOrigin: true,
-      // },
-      // "/sanctum": {
-      //   target: "http://localhost/tracert/backend/public_html",
-      //   changeOrigin: true,
-      // },
       "/api": {
-        target: "https://tracerapp.uiidalwa.web.id",
+        target: "http://localhost:8000",
         changeOrigin: true,
       },
       "/sanctum": {
-        target: "https://tracerapp.uiidalwa.web.id",
+        target: "http://localhost:8000",
         changeOrigin: true,
       },
     },
